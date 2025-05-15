476e292a5d01d72578c0267744d5e244443241dd
Abdelrahman844 <Bodyakram7@gmail.com>
2025-05-16 01:22:18 +0300

    Added some .axaml pages

---
+++ MyAvaloniaApp/Views/ClientProfile.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyAvaloniaApp.Views
+{
+    public partial class ClientProfile : Window
+    {
+        public ClientProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
