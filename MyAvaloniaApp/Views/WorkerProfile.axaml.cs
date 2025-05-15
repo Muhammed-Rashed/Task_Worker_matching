9a3eb8caaadcbe0bc0926fed8afe9d15b7d95446
Abdelrahman844 <Bodyakram7@gmail.com>
2025-05-16 02:09:41 +0300

    Added some .axaml pages

---
+++ MyAvaloniaApp/Views/WorkerProfile.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyAvaloniaApp.Views
+{
+    public partial class WorkerProfile : Window
+    {
+        public WorkerProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
