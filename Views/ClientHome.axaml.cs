e3f5e834f437240fb7a015b70e8605d9eb22f1d7
Abdelrahman844 <Bodyakram7@gmail.com>
2025-05-16 00:44:11 +0300

    Added some .axaml pages

---
+++ Views/ClientHome.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyAvaloniaApp.Views
+{
+    public partial class ClientHome : Window
+    {
+        public ClientHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/Login.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyAvaloniaApp.Views
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
