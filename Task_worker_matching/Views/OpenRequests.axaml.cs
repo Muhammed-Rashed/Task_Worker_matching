c7d6f8f4a279ded184e0b9c2797a05cb343f72e3
Abdelrahman844 <Bodyakram7@gmail.com>
2025-05-16 20:53:55 +0300

    Added some pages

---
+++ Task_worker_matching/Views/OpenRequests.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyAvaloniaApp.Views
+{
+    public partial class OpenRequests : Window
+    {
+        public OpenRequests()
+        {
+            InitializeComponent();
+        }
+    }
+}
