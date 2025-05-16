a9354c1ff9937c8a208941ad813aa6f1b66589a4
Abdelrahman844 <Bodyakram7@gmail.com>
2025-05-16 18:52:44 +0300

    Added my requests page and some icons

---
+++ Task_worker_matching/Views/MyRequests.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyAvaloniaApp.Views
+{
+    public partial class MyRequests : Window
+    {
+        public MyRequests()
+        {
+            InitializeComponent();
+        }
+    }
+}
