a53b26aee9cf2f21cc525f5ae5259baf679c7454
morphy0123 <161810895+morphy0123@users.noreply.github.com>
2025-05-16 19:54:51 +0300

    Add files via upload

---
+++ Task_worker_matching/ServiceLayer/Cache.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+class Cache<T>
+{
+    private IMemory<T> memory;
+    private IRepositryStrategy<T> repositry;
+    private IRepositryFactory<T> factory;
+    
+    public bool add(T item)
+    {
+        return memory.add(item) && IRepositryStrategy.add(item) && IRepositryFactory.add(item);
+    }
+    
+    public bool update(T new_item, T old_item)
+    {
+        return memory.update(T new_item, T old_item) && 
+               IRepositryStrategy.update(T new_item, T old_item) && 
+               IRepositryFactory.update(T new_item, T old_item);
+    }
+    
+    public bool delete(T item)
+    {
+        return memory.delete(item) && IRepositryStrategy.delete(item) && IRepositryFactory.delete(item);
+    }
+    
+    public List<T> get_data()
+    {
+        if(!memory.isEmpty())
+            return memory.get_data();
+        
+        if(!repositry.isEmpty())
+        {
+            memory.set_data(repositry.get_data());
+            return repositry.get_data();
+        }
+        
+        if(!factory.isEmpty())
+        {
+            memory.set_data(factory.get_data());
+            repositry.set_date(factory.get_data());
+            return repositry.get_data();
+        }
+        
+        return null;
+    }
+}
