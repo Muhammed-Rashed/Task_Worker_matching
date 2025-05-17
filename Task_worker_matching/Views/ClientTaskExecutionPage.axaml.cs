ad5968cae59485e5d0848d0321891a93e957dfca
Muhammed-Rashed <mohamed.rashedwork2005@gmail.com>
2025-05-17 18:31:10 +0300

    ...

---
+++ Task_worker_matching/Views/ClientTaskExecutionPage.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MyAvaloniaApp.Views;
+
+public partial class ClientTaskExecutionPage : UserControl
+{
+    public ClientTaskExecutionPage()
+    {
+        InitializeComponent();
+    }
+}
