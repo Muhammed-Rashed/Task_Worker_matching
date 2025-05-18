c37ebe2a7d11017d354eef02dd48d3edd6de8aeb
Muhammed-Rashed <mohamed.rashedwork2005@gmail.com>
2025-05-18 10:41:34 +0300

    Added the Navgeitor

---
+++ Task_worker_matching/Controllers/Navigator.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Controls;
+
+namespace Task_worker_matching.Controllers
+{
+    public class Navigator
+    {
+        private static Navigator? _instance;
+        public static Navigator Instance => _instance ??= new Navigator();
+
+        private ContentControl? _mainContentControl;
+
+        public void SetContentControl(ContentControl contentControl)
+        {
+            _mainContentControl = contentControl;
+        }
+
+        public void Navigate(UserControl page)
+        {
+            if (_mainContentControl != null)
+            {
+                _mainContentControl.Content = page;
+            }
+        }
+    }
+}
